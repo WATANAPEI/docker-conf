3a8dcf84220f2ef451c102bf1cb772b8ae8ec52e
WATANAPEI <pei2791150@gmail.com>
2019-05-28 10:44:20 +0000

    add build option

--- start.sh
+++ start.sh
@@ -9,7 +9,7 @@ docker volume create out
 docker network inspect mynet &> /dev/null || docker network create mynet
 
 # start p-build service
-docker-compose -f ../p-drum/docker-compose.yml up -d
+docker-compose -f ../p-drum/docker-compose.yml up -d --build
 # start proxy service
-docker-compose -f ../proxy/docker-compose.yml up -d
+docker-compose -f ../proxy/docker-compose.yml up -d --build
 
