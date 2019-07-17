52b9ee0f5ad3471ba5135211908d30ddabba31d6
WATANAPEI <info@wpei.dev>
2019-07-17 19:32:17 +0000

    add start and close script

--- close.sh
+++ close.sh
@@ -2,15 +2,20 @@
 # initialize script for my service
 # Usage: sh ./init.sh
 
-# start p-build service
+# close lang-frontend service
+docker-compose -f ../lang-frontend/docker-compose.yml down
+# close p-build service
 docker-compose -f ../p-drum/docker-compose.yml down
 
-# start proxy service
+# close proxy service
 docker-compose -f ../proxy/docker-compose.yml down
+# close lang service
+docker-compose -f ../lang/docker-compose.yml down
 
 # define volume and network
 # if exists, just skip
 docker volume rm out
+docker volume rm out_lang-frontend
 
 docker network rm mynet
 
--- start.sh
+++ start.sh
@@ -5,11 +5,16 @@
 # define volume and network
 # if exists, just skip
 docker volume create out
+docker volume create out_lang-frontend
 
 docker network inspect mynet &> /dev/null || docker network create mynet
 
+# start lang service
+docker-compose -f ../lang/docker-compose.yml up -d --build
 # start proxy service
 docker-compose -f ../proxy/docker-compose.yml up -d --build
 # start p-build service
 docker-compose -f ../p-drum/docker-compose.yml up -d --build
+# start lang-frontend service
+docker-compose -f ../lang-frontend/docker-compose.yml up -d --build
 
